d1d56ebd736b061e6ea272d82fb5dd05229cf2e4
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-04-23 22:58:21 -0400

    Bump airflow-pydantic version for full semver

--- airflow_config/tests/setups/good/complex/test_complex.py
+++ airflow_config/tests/setups/good/complex/test_complex.py
@@ -1,7 +1,10 @@
+from unittest.mock import patch
+
 from airflow_config import load_config
 
 
 def test_config_and_options():
-    conf = load_config("config", "config")
+    with patch("airflow_balancer.config.balancer.Pool"):
+        conf = load_config("config", "config")
 
-    assert str(conf.model_dump_json(indent=2, serialize_as_any=True))
+        assert str(conf.model_dump_json(indent=2, serialize_as_any=True))
