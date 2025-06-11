310eddd6fd0e3469d6d20a6a4866e1f38d69c990
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-11 13:25:49 -0400

    Tweak reexports, clean up setups tests

--- airflow_config/tests/setups/good/basic/test_complex.py
+++ airflow_config/tests/setups/good/basic/test_complex.py
@@ -5,6 +5,6 @@ from airflow_config import load_config
 
 def test_config_and_options():
     with patch("airflow_balancer.config.balancer.Pool"):
-        conf = load_config("config", "config")
+        conf = load_config("config", "complex")
 
         assert str(conf.model_dump_json(indent=2, serialize_as_any=True))
--- airflow_config/tests/setups/good/specialize/test_dag_disable.py
+++ airflow_config/tests/setups/good/specialize/test_dag_disable.py
@@ -5,7 +5,16 @@ from airflow_config import DAG, load_config
 
 def test_create_dag_from_config():
     with patch("sys.exit") as sys_exit:
-        conf = load_config("config", "test")
+        conf = load_config("config", "disable")
+        DAG(dag_id="testdag", config=conf)
+        assert sys_exit.call_count == 1
+        DAG(dag_id="example_dag", config=conf)
+        assert sys_exit.call_count == 2
+
+
+def test_create_dag_from_config_disable_perdag():
+    with patch("sys.exit") as sys_exit:
+        conf = load_config("config", "disable_perdag")
         DAG(dag_id="testdag", config=conf)
         assert sys_exit.call_count == 1
         DAG(dag_id="example_dag", config=conf)
