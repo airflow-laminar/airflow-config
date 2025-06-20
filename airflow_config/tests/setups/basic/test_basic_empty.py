8dfde2a2e819337d8f602654a908abb8d921116b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-20 16:38:11 -0400

    Handle explicit none, fixes #41

--- airflow_config/tests/setups/basic/test_basic_empty.py
+++ airflow_config/tests/setups/basic/test_basic_empty.py
@@ -4,7 +4,7 @@ from airflow_config import create_dag, load_config
 def test_config_and_empty_file():
     conf = load_config("config", "basic")
     assert conf.default_args.owner == "test"
-    assert str(conf.model_dump_json(indent=2, serialize_as_any=True))
+    assert str(conf.model_dump_json(exclude_unset=True, indent=2, serialize_as_any=True))
 
 
 def test_config_and_empty_file_create_dag():
--- airflow_config/tests/setups/basic/test_complex.py
+++ airflow_config/tests/setups/basic/test_complex.py
@@ -7,4 +7,15 @@ def test_config_and_options():
     with patch("airflow_balancer.config.balancer.Pool"):
         conf = load_config("config", "complex")
 
-        assert str(conf.model_dump_json(indent=2, serialize_as_any=True))
+        assert str(conf.model_dump_json(exclude_unset=True, indent=2, serialize_as_any=True))
+
+
+def test_config_null_schedule():
+    with patch("airflow_balancer.config.balancer.Pool"):
+        conf = load_config("config", "complex")
+
+        assert "none-schedule" in conf.dags
+        assert conf.dags["none-schedule"].schedule is None
+
+        inst = conf.dags["none-schedule"].instantiate()
+        assert inst.schedule_interval is None
