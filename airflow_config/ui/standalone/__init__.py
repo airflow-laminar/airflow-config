ab2982ac232117a6d9c5895eac5a3cda0923d700
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-03-30 13:41:33 -0400

    Updates for starlette 1, allow plugin failure

--- airflow_config/ui/standalone/__init__.py
+++ airflow_config/ui/standalone/__init__.py
@@ -54,7 +54,6 @@ def build_app() -> FastAPI:
             Path(__file__).parent / "templates",
             Path(flask_appbuilder_root).parent / "templates",
         ],
-        autoescape=True,
     )
 
     # Mount top level routes
