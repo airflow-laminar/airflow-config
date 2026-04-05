c54e9fd6178e4578dc4e258812094f86aa5b37ea
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-04-05 18:31:48 -0400

    fix conflicts

--- js/build.mjs
+++ js/build.mjs
@@ -3,10 +3,6 @@ import { transform } from "lightningcss";
 import { getarg } from "./tools/getarg.mjs";
 import fs from "fs";
 import cpy from "cpy";
-<<<<<<< before updating
-import { createRequire } from "node:module";
-=======
->>>>>>> after updating
 
 const DEBUG = getarg("--debug");
 
@@ -29,11 +25,6 @@ const BUILD = [
   },
 ];
 
-<<<<<<< before updating
-const require = createRequire(import.meta.url);
-function add(builder, path, path2) {
-  builder.add(path, fs.readFileSync(require.resolve(path2 || path)).toString());
-=======
 async function compile_css() {
   const process_path = (path) => {
     const outpath = path.replace("src/css", "dist/css");
@@ -59,22 +50,6 @@ async function compile_css() {
   };
 
   process_path("src/css");
->>>>>>> after updating
-}
-
-async function compile_css() {
-  const builder1 = new BuildCss("");
-  add(builder1, "./src/less/index.less");
-  // add(
-  //   builder1,
-  //   "shoelace_light.css",
-  //   "@shoelace-style/shoelace/dist/themes/light.css",
-  // );
-
-  const css = builder1.compile().get("index.css");
-
-  fs.mkdirSync("dist/css", { recursive: true });
-  fs.writeFileSync("dist/css/index.css", css);
 }
 async function copy_to_python() {
   fs.mkdirSync("../airflow_config/ui/static", { recursive: true });
